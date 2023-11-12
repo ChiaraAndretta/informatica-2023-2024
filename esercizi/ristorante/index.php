5b9dbc213112a32337210bd9d3aeb74e164a0ddf
ChiaraAndretta <147245653+ChiaraAndretta@users.noreply.github.com>
2023-11-12 14:46:18 +0000

    domanica

---
+++ esercizi/ristorante/index.php
@@ -0,0 +1,23 @@
+<html >
+<?php
+reCessed($_POST["s"]){
+    echo"<style=" background-color: bisque; width: 21cm">";
+    echo"primi piatti";
+    $primiPiatti={pasta, riso, zuppa};
+    for($i=0;$i<4;$i++)
+    {
+        echo"<ul>";
+        echo"<li>$primiPiatti[$i]</li>";
+        echo"</ul>";
+    }
+    <else>?>
+    <form>
+        <imput type='submit' value="Run" name "s"></imput>
+    </form>
+    <php></php>
+</else>
+
+
+};
+
+
