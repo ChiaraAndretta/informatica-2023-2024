f3402381b316b70144e3190228612d470ed4b973
ChiaraAndretta <147245653+ChiaraAndretta@users.noreply.github.com>
2023-12-02 11:56:26 +0000

    SCUOLA

---
+++ esercizi/questionarioù/accesso_pagina_benvenuto.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dati Utente Registrato</title>
+    <style>
+        body {
+            <?php
+                $coloreSfondo = $_GET['Colore'];
+                echo "background-color: $coloreSfondo;";
+            ?>
+        }
+        table {
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+            padding: 8px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        foreach ($_GET as $k => $v) {
+    ?>
+    <h1>Benvenuto <?= $_GET['Nome'] ?> </h1>
+    <h2>Dati Utente Registrato</h2>
+    <table>
+        <tr>
+            <th>Campo</th>
+            <th>Valore</th>
+        </tr>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            foreach ($_GET as $k => $v) {
+        ?>
+            <tr>
+                <td>
+                    <?= $k ?>
+                </td>
+                <td>
+                    <?= $v ?>
+                </td>
+            </tr>
+
+        <?php
+            }
+        }
+        ?>
+        <?php
+            }
+        }
+        ?>
+    </table>
+</body>
+</html>
